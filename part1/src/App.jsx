84975279f795248797cbf959389a079185f3dc7b
Tani1964 <ifegbesan6@gmail.com>
2023-10-01 14:06:32 +0100

    b_exercises

---
+++ part1/src/App.jsx
@@ -0,0 +1,29 @@
+import Content from "./components/Content"
+import Header from "./components/Header"
+import Total from "./components/Total"
+
+const App = () => {
+  const course = 'Half Stack application development'
+  const part1 = {
+    name: 'Fundamentals of React',
+    exercises: 10
+  }
+  const part2 = {
+    name: 'Using props to pass data',
+    exercises: 7
+  }
+  const part3 = {
+    name: 'State of a component',
+    exercises: 14
+  }
+
+  return (
+    <div>
+      <Header course={course} />
+      <Content exercises={[part1.exercises,part2.exercises, part3.exercises]} parts={[part1.name, part2.name, part3.name]} />
+      <Total exercises={[part1.exercises,part2.exercises, part3.exercises]}/>
+    </div>
+  )
+}
+
+export default App
